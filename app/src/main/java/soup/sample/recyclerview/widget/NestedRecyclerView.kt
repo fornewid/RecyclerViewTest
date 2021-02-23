e94f47a63b96a00816b9f63f36ae6afc8460d3e2
SOUP <soupyong@gmail.com>
2021-02-23 23:48:54 +0900

    Improves scrolling change between horizontal and vertical list view.

---
+++ app/src/main/java/soup/sample/recyclerview/widget/NestedRecyclerView.kt
@@ -0,0 +1,39 @@
+package soup.sample.recyclerview.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+
+class NestedRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    private val itemTouchListener = object : RecyclerView.OnItemTouchListener {
+
+        override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+            if (e.action == MotionEvent.ACTION_DOWN) {
+                rv.stopScroll()
+            }
+            return false
+        }
+
+        override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        }
+
+        override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        addOnItemTouchListener(itemTouchListener)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        removeOnItemTouchListener(itemTouchListener)
+    }
+}
